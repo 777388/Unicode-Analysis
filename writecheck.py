dfb26344fd51b9fcc0b328143e3c1f067b7fbb0b
Christopher Lycan <96343159+777388@users.noreply.github.com>
2023-12-28 13:55:15 -0800

    Add files via upload

---
+++ writecheck.py
@@ -0,0 +1,11 @@
+import sys
+class instantiate:
+    instance = []
+for i in range(1114112):
+    if (i > 55295 & i < 57348):
+        pass
+    else:
+        instantiate.instance.append(chr(i))
+with open(sys.argv[1], "w") as g:
+    topography = lambda node: print(str(g.write(node)) + node, end="") 
+    (list(map(topography, instantiate.instance)))
